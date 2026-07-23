bf6add5dc157b1d1aa595f51af289d067651aabc
Shoutoo <sofinhalim230@gmail.com>
2026-07-23 23:52:58 +0700

    chore: make project fully render web service compatible

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -8,9 +8,10 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         apiPrefix: 'api',
-        health: '/up',
+        health: '/health',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
